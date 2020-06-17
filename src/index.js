04a0bbc68aa3374e4be3bf7263b846b88aaeaa18
Dowland Aiello <mitsukomegumii@gmail.com>
2020-06-16 22:30:08 -0400

    js-core-test: Update tests to include pk pretty defs.
    Signed-off-by: Dowland Aiello <mitsukomegumii@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,4 +5,5 @@ console.log(keypair.json_pretty());
 
 const acc = Account.from_keypair(keypair);
 console.log("\nRespective account: ");
-console.log(acc.json_pretty());
+console.log(`Private key: ${acc.keypair.private_key_pretty()}`);
+console.log(`Public key: ${acc.keypair.public_key_pretty()}`);
