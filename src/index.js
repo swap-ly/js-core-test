d632f544c146d15b4b184632d5d37c2ce332748c
Dowland Aiello <dowlandaiello@gmail.com>
2020-06-28 14:38:41 -0400

    js-core-test: Remove redundant index.js file.
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const { Account, Keypair } = require('@swaply/core-node');
-
-const keypair = Keypair.generate();
-console.log(keypair.json_pretty());
-
-const acc = Account.from_keypair(keypair);
-console.log('\nRespective account: ');
-console.log(`Private key: ${acc.keypair.private_key_pretty()}`);
-console.log(`Public key: ${acc.keypair.public_key_pretty()}`);
